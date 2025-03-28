fb011b700d79bfabcc03a4de596cb28aec8493ac
sneaky <xsneakydev@gmail.com>
2025-03-28 13:50:57 +0100

    music

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 function getRandomImage() {
   const images = [
-    'myszery/mysz1.jpg',
-    'myszery/mysz2.jpg',
-    'myszery/mysz3.jpg',
-    'myszery/mysz4.jpg',
-    'myszery/mysz5.jpg',
-    'myszery/mysz6.jpg',
-    'myszery/mysz7.jpg',
-    'myszery/mysz8.jpg',
-    'myszery/mysz9.jpg',
-    'myszery/mysz10.jpg',
-    'myszery/mysz11.jpg',
-    'myszery/mysz12.jpg',
-    'myszery/mysz13.jpg'
+    'myszery/mysz1.jpg', 'myszery/mysz2.jpg', 'myszery/mysz3.jpg', 'myszery/mysz4.jpg', 'myszery/mysz5.jpg',
+    'myszery/mysz6.jpg', 'myszery/mysz7.jpg', 'myszery/mysz8.jpg', 'myszery/mysz9.jpg', 'myszery/mysz10.jpg',
+    'myszery/mysz11.jpg', 'myszery/mysz12.jpg', 'myszery/mysz13.jpg'
   ];
-
   const randomIndex = Math.floor(Math.random() * images.length);
   return images[randomIndex];
 }
@@ -26,10 +15,14 @@ function setRandomImage() {
 }
 
 let discoInterval;
+let audio = new Audio('audio.mp3');
+audio.loop = true;
+
 function toggleDiscoMode() {
   if (document.getElementById('disco-mode').checked) {
     document.body.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
     setRandomImage();
+    audio.play();
     discoInterval = setInterval(() => {
       document.body.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
       setRandomImage();
@@ -37,12 +30,14 @@ function toggleDiscoMode() {
   } else {
     clearInterval(discoInterval);
     document.body.style.backgroundColor = '#121212';
+    audio.pause();
+    audio.currentTime = 0;
   }
 }
 
 window.onload = () => {
   setRandomImage();
-  document.getElementById('disco-mode').checked=false;
+  document.getElementById('disco-mode').checked = false;
 }
 
 document.getElementById('randomize-btn').addEventListener('click', setRandomImage);
